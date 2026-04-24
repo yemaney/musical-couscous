959043055a7997897b196f4758f5dde2c2cfd5fe
yemaney <yemane_@outlook.com>
2026-04-24 21:37:09 +0000

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/musical-couscous',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
